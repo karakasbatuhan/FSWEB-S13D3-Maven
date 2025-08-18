908d5ca1498f16b2e19b6d893fb6efd1c6d9e4a8
Batuhan Karakaş <karakasbatuhan96@gmail.com>
2025-08-18 15:27:46 +0300

    Done.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Person person = new Person("John", "Doe", 20);
 
+        System.out.println("Person outcomes: ");
         System.out.println("Firstname: " + person.getFirstName());
-
         System.out.println("LastName: " + person.getLastName());
-
         System.out.println("Age: " + person.getAge());
+
+        System.out.println(" ");
+
+        System.out.println("Wall outcomes: ");
+        Wall wall = new Wall(5,4);
+
+        System.out.println("area= " + wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
     }
 }
