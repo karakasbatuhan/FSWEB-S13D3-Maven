404c166eb1c0df5b53b96ceed2fd01d6e74f3e98
Batuhan Karakaş <karakasbatuhan96@gmail.com>
2025-08-18 16:46:40 +0300

    Done.

--- src/main/java/org/example/Wall.java
+++ src/main/java/org/example/Wall.java
@@ -17,24 +17,22 @@ public class Wall {
         return this.height;
     }
 
-    public double setWidth(double width){
+    public void setWidth(double width){
         if(width > 0){
             this.width = width;
         }
         else{
             this.width = 0;
         }
-        return this.width;
     }
 
-    public double setHeight(double height){
+    public void setHeight(double height){
         if(height > 0){
             this.height = height;
         }
         else{
             this.height = 0;
         }
-        return this.height;
     }
 
     public double getArea(){
